2a234b5c8d1978db4f897cd0231002507a47d3ba
vivekkoya <13vivekkoya@gmail.com>
2023-12-30 14:53:43 -0800

    Add Image composable for displaying graphical information in our application

--- app/src/main/java/com/vmtechllc/design/MainActivity.kt
+++ app/src/main/java/com/vmtechllc/design/MainActivity.kt
@@ -3,6 +3,7 @@ package com.vmtechllc.design
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,6 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -79,3 +82,9 @@ fun NameInput(){
     TextField(value = textState.value, onValueChange = {newValue -> textState.value = newValue},
         label = { Text(text = "Your name")})
 }
+@Preview(showBackground = true)
+@Composable
+fun BeautifulImage(){
+    Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = "My App Icon", contentScale = ContentScale.Fit)
+}
+
